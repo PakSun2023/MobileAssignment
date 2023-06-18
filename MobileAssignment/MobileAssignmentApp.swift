c47b8bf59d27879ecef96f5af3c195505e1429c0
KT Chan <kt.2morrow@gmail.com>
2023-06-18 20:13:54 +0800

    Initial Commit

---
+++ MobileAssignment/MobileAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileAssignmentApp.swift
+//  MobileAssignment
+//
+//  Created by KayTee Chan on 18/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct MobileAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
